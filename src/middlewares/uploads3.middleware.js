f99c0006ca803625dce53d44076c82908983d261
Tanh101 <lyvantanh101@gmail.com>
2024-04-18 16:14:09 +0700

    create upload multiple files function  with s3

---
+++ src/middlewares/uploads3.middleware.js
@@ -0,0 +1,52 @@
+const multer = require('multer');
+const path = require('path');
+const { S3Client } = require('@aws-sdk/client-s3')
+
+const multerS3 = require('multer-s3')
+
+const s3 = new S3Client({
+    credentials: {
+        secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+        accessKeyId: process.env.AWS_ACCESS_KEY_ID
+    },
+    region: process.env.AWS_REGION
+})
+
+
+const storage = multerS3({
+    s3: s3,
+    bucket: process.env.S3_BUCKET_NAME,
+    contentType: multerS3.AUTO_CONTENT_TYPE,
+    metadata: function (req, file, cb) {
+        cb(null, { fieldName: file.fieldname });
+    },
+    key: function (req, file, cb) {
+        cb(null, Date.now().toString())
+    }
+})
+
+function checkFileType(file, cb) {
+    const filetypes = /jpeg|jpg|png|gif|mp4|mov|png/;
+
+    const extname = filetypes.test(path.extname(file.originalname).toLowerCase());
+
+    const mimetype = filetypes.test(file.mimetype);
+    
+
+    if (extname && mimetype) {
+      return cb(null, true);
+    } else {
+      cb('Error: Images only (jpeg, jpg, png, gif, mp4, mov, png)!');
+    }
+  }
+
+  const upload = multer({
+    storage: storage,
+    fileFilter: function (req, file, cb) {
+      checkFileType(file, cb);
+    },
+  }); 
+
+const uploadMiddleWare = upload
+
+module.exports = uploadMiddleWare
